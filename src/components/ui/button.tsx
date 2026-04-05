11292cf7d2da35e91ae72227222c3345b169a918
Phan ngoc thuong <nt.6924.thuong@gmail.com>
2026-04-06 06:16:48 +0700

    fix: update button component with @base-ui/react and fix dependencies

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button as ButtonPrimitive } from "@base-ui/react/button"
+import { Button as ButtonPrimitive } from "@base-ui/react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
